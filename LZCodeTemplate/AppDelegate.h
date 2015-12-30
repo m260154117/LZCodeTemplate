dbf9debfccd4b4ebfba8a1208182ce34bd2600d2
m260154117 <260154117@qq.com>
2015-12-30 11:59:27 +0800

    上传项目

---
+++ LZCodeTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZCodeTemplate
+//
+//  Created by MiaoLizhuang on 15/12/30.
+//  Copyright © 2015年 MiaoLizhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZCodeTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZCodeTemplate
+//
+//  Created by MiaoLizhuang on 15/12/30.
+//  Copyright © 2015年 MiaoLizhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
